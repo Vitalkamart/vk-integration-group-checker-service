36269802a77d4beb9e8d7e10fdcf429c8361b5d6
vitalkamart <vitalkamart@gmail.com>
2025-08-25 19:51:38 +0300

    OpenApiConfig
    Added OpenApiConfig

--- src/main/java/ru/mart/vkservice/infrastructure/controller/VkUserController.java
+++ src/main/java/ru/mart/vkservice/infrastructure/controller/VkUserController.java
@@ -2,9 +2,11 @@ package ru.mart.vkservice.infrastructure.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,18 +29,43 @@ public class VkUserController {
 
     @Operation(
             summary = "Получить информацию о пользователе VK",
-            description = "Возвращает ФИО пользователя и информацию о членстве в группе"
+            description = "Возвращает ФИО пользователя и информацию о членстве в группе",
+            security = @SecurityRequirement(name = "basicAuth")
     )
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Успешный ответ",
-                    content = @Content(schema = @Schema(implementation = VkUserResponseDto.class))),
+                    content = @Content(schema = @Schema(implementation = VkUserResponseDto.class),
+                            examples = @ExampleObject(value = """
+                                    {
+                                      "lastName": "Иванов",
+                                      "firstName": "Иван",
+                                      "middleName": "Иванович",
+                                      "member": true
+                                    }
+                                    """))),
             @ApiResponse(responseCode = "400", description = "Неверные параметры запроса"),
             @ApiResponse(responseCode = "401", description = "Не авторизован"),
-            @ApiResponse(responseCode = "500", description = "Ошибка сервера")
+            @ApiResponse(responseCode = "500", description = "Ошибка сервера или VK API")
     })
     @PostMapping("/info")
     public ResponseEntity<VkUserResponseDto> getUserInfo(
-            @RequestHeader("vk_service_token") String serviceToken,
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Данные для запроса",
+                    required = true,
+                    content = @Content(
+                            schema = @Schema(implementation = VkUserRequestDto.class),
+                            examples = @ExampleObject(value = """
+                                    {
+                                      "user_id": 12345,
+                                      "group_id": 67890
+                                    }
+                                    """)
+                    )
+            )
+            @RequestHeader("vk_service_token")
+            @Schema(description = "Сервисный токен VK приложения", example = "vk1234567890abcdef")
+            String serviceToken,
+
             @Valid @RequestBody VkUserRequestDto request) {
 
         log.info("Received request for user ID: {}, group ID: {}", request.getUserId(), request.getGroupId());
